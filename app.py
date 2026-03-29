1cb3a5771ddff7a94f1c271b20818e58d2a324cc
Beverly Ajuzie <beverlyajuzie@MacBookAir.lan>
2026-03-29 03:48:04 -0500

    updated dashboard design

--- app.py
+++ app.py
@@ -190,7 +190,7 @@ input_df = pd.DataFrame([{
     "CreditCard": credit,
 }])
 
-left_side, right_side = st.columns([2, 1])
+left_side, right_side = st.columns([3, 1])
 
 # LEFT HALF
 with left_side:
