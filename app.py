e34cb0d76745d785ac3a1afae3cc70c692b2f830
Beverly Ajuzie <beverlyajuzie@MacBookAir.lan>
2026-03-29 03:01:13 -0500

    updated dashboard design

--- app.py
+++ app.py
@@ -190,7 +190,7 @@ input_df = pd.DataFrame([{
     "CreditCard": credit,
 }])
 
-outer_left, center, outer_right = st.columns([1, 7, 0.6])
+outer_left, center, outer_right = st.columns([0.4, 7, 0.2])
 
 with center:
     col1, col2, col3, col4 = st.columns(4, gap="medium")
